7bf90ae2a23d3750042423136844a475478ac90c
Aditya Morankar <47907947+adi112100@users.noreply.github.com>
2020-03-15 16:56:13 +0530

    Add files via upload

---
+++ flower.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flower : MonoBehaviour
+{
+    public float n,d,scale,times;
+    float k;
+    float theta;
+    float r, x, y;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        k = n / d;
+        r = Mathf.Cos(k * theta)*scale;
+        x = r*Mathf.Cos(theta);
+        y = r*Mathf.Sin(theta);
+
+        transform.position = new Vector3(x*20, y*20, 0);
+        
+        theta += times*0.1f * Time.deltaTime;
+        if (theta > 9223372036854775295) theta = 0f;
+        if(theta < 0.1f*times)
+        {
+            gameObject.GetComponent<TrailRenderer>().enabled = false;
+        }
+        else gameObject.GetComponent<TrailRenderer>().enabled = true;
+
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,5 @@
+This is code to create amazing design based on Rose(mathematics)
+base on value of r(radius) how amzing designs are produce
+
+this code is especially for unity 
+written in c#
